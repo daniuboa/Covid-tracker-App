296775054ccae2f20b1271e702803e42e289c2bd
Daniel Uboa <uboadaniel@gmail.com>
2022-01-25 14:40:04 +0300

    new changes

---
+++ covid-tracker/src/components/CountrySelect.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <select 
+        @change="onChange"
+        v-model="selected"
+        class="form-select mt-10 block w-full border p-3 rounded">
+        <option value="0">Select Country</option>
+        <option v-for="country in countries" :value="country.ID"> 
+            {{ country.Country }} 
+        </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: ['countries'],
+    data() {
+        return {
+            selected: 0,
+        }
+    },
+    methods: {
+        onChange(e) {
+            const country = this.countries.find((item) => item.ID === this.selected)
+
+            console.log(country)
+
+            this.$emit('get-country', country)
+        },
+    },
+}
+</script>
---
+++ covid-tracker/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i>Covid-19 Tracker
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
